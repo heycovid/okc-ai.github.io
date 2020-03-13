7be46de5f9a8c7a415b025285601286d294156f3
Ben Holmes <ben@Bens-MacBook-Pro.local>
2020-03-13 16:42:16 -0400

    ads amplify & web build

--- web-build/register-service-worker.js
+++ web-build/register-service-worker.js
@@ -3,7 +3,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker
-      .register('/okc-ai.github.io/expo-service-worker.js', { scope: '/okc-ai.github.io/' })
+      .register('/expo-service-worker.js', { scope: '/' })
       .then(function(info) {
         // console.info('Registered service-worker', info);
       })
